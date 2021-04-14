67a73724b2382e0a4d8f71e7d6279ce725975906
christy9703 <46849186+christy9703@users.noreply.github.com>
2021-04-14 15:28:57 -0500

    Completed faust_stream.py

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -33,16 +33,16 @@ class TransformedStation(faust.Record):
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("org.chicago.cta.connect-stations", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("org.chicago.cta.stations", partitions=1)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+    "table-stations",
+     default=str,
+     partitions=1,
+     changelog_topic=out_topic,
+)
 
 
 #
@@ -52,7 +52,16 @@ app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memor
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
 #
 #
-
+@app.agent(topic)
+async def process(stations):
+    transformed_station=TransformedStation(
+        station_id=station.station_id,
+        station_name=station.station_name,
+        order=station.order,
+        line="green" if station.green else (
+            "blue" if station.blue else "red"
+        )
+    )
 
 if __name__ == "__main__":
     app.main()
